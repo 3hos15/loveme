50fcceaf1c0814921498ce4ba11971cdaea8fcd7
3hos15 <145386421+3hos15@users.noreply.github.com>
2024-05-23 18:29:41 +0200

    b

--- script.js
+++ script.js
@@ -1,45 +1,27 @@
-console.log("Love Me")
 
-// let health = document.querySelector("#health") //Vraag get element by ID
-// health.value -= 30;
-
-// const btn = document.querySelector("button")
-
-// let textToType = "Welcome to LOVE ME. With LOVE ME you can train your... loving skills! ";
-// let charIndex = 0;
-// let typingSpeed = 100;
-// let outputElement = document.getElementById('typing-effect');
-
-// function typeText() {
-//     if (charIndex < textToType.length) {
-//         outputElement.textContent += textToType.charAt(charIndex);
-//         charIndex++;
-//     } else {
-//         clearInterval(typingInterval);
-//     }
-// }
-
-// let typingInterval = setInterval(typeText, typingSpeed);
-
-// btn.addEventListener("click", typeText)
+console.log("Love Me");
 
 document.addEventListener('DOMContentLoaded', function() {
-    let textToType = "Welcome to LOVE ME. With LOVE ME you can train your... loving skills! Click on the button (yes the big one down there) to start your loving journey :).";
-    let charIndex = 0;
+    let welcomeText = "Welcome to LOVE ME. With LOVE ME you can train your... loving skills! Click on the button (yes the big one down there) to start your loving journey :).";
+    let aantalChar = 0;
     let typingSpeed = 100;
-    let outputElement = document.querySelector('#starttekst');
+    let gameText = document.querySelector('#starttekst');
+    let gameLink = document.querySelector('.knop');
 
     function typeText() {
-        if (charIndex < textToType.length) {
-            outputElement.textContent += textToType.charAt(charIndex);
-            charIndex++;
+        if (aantalChar < welcomeText.length) {
+            gameText.textContent += welcomeText.charAt(aantalChar);
+            // gameText.textContent = gameText.textContent + welcomeText.charAt(aantalChar);
+            aantalChar++;
+            // aantalChar = aantalChar + 1;
         } else {
             clearInterval(typingInterval);
+            gameLink.style.display = 'flex'; 
+            console.log("You can continue to the game!")
         }
     }
 
     let typingInterval = setInterval(typeText, typingSpeed);
 
-    // Start the typing effect immediately after the page loads
     typeText();
 });
